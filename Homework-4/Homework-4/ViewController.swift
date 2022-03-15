b4ba4869b8378539888b26676cbd28f90110250f
kirill-skr <100729335+kirill-skr@users.noreply.github.com>
2022-03-15 23:37:25 +0300

    fixed the comments

--- Homework-4/Homework-4/ViewController.swift
+++ Homework-4/Homework-4/ViewController.swift
@@ -13,40 +13,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-      arrayOfTupleInt([1, 2, 3, 4, 5, 6, 7, 8, 9], ["one", "two"])
-
-}
-    func arrayOfTupleInt(_ number: [Int],_ string: [String]) {
-        for indexForNumbers in number.indices {
-            print(indexForNumbers)
+    let arrayOfTupleInt = [("one", 6), ("two", 8)]
+        
+    for indexForNumbers in 0..<arrayOfTupleInt.count {
+        let element = arrayOfTupleInt[indexForNumbers]
+        let number = element.1
+        let sum = number * number
+        print(sum)
+        
+    }
+        var even1 = [(String, Int)]()
+        for even in arrayOfTupleInt  {
+            if even.1 % 2 == 0 {
+            even1.append(even)
+            }
         }
-        for indexForSring in string.indices {
-            print(indexForSring)
-        }
-      let square = number
-      for sumOfsquare in square {
-      var squaring = sumOfsquare
-          squaring *= squaring
-          
-        print(squaring)
+    }
 }
-        for indexForNumbers in number.indices {
-            print(indexForNumbers)
-        }
-        for indexForString in string.indices {
-            print(indexForString)
-        }
-      for even in number {
-      if even % 2 == 0 {
-          print(even)
-      }
-          let filterArray = number.filter { (item) -> Bool in
-              item % 2 == 0
-}
-          print(filterArray)
-}
-}
-}
-
-
 
