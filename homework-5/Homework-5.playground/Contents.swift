2a676357a6b9c4f9a8e0a4ae0fee8aa26f7bc9aa
kirill-skr <100729335+kirill-skr@users.noreply.github.com>
2022-03-21 04:23:30 +0300

    fixed the comments

--- homework-5/Homework-5.playground/Contents.swift
+++ homework-5/Homework-5.playground/Contents.swift
@@ -1,35 +1,38 @@
 import UIKit
 
-var country: [String: String]
-country = [
-    "Germany": "Western Europe. The surname Hitler is banned in Germany",
-    "Italy": "Southern Europe. The ending of all words in Italian only with a vowel",
-    "France": "Western Europe. France is the birthplace of the bicycle",
+//1
+
+var countrys = [
+    "Germany": ("Western Europe", "The surname Hitler is banned in Germany"),
+    "Italy": ("Southern Europe", "The ending of all words in Italian only with a vowel"),
+    "France": ("Western Europe", "France is the birthplace of the bicycle"),
 ]
 
-country["Italy"] = nil
-country["Spain"] = "located on the Iberian Peninsula. Football is considered the national sport"
-country["Germany"] = "The first book was printed in Germany"
-country["France"] = "France is the birthplace of cinema"
-print(country)
+countrys["Italy"] = nil
+countrys["Spain"] = ("located on the Iberian Peninsula", "Football is considered the national sport")
+countrys["Germany"] = ("Western Europe", "The first book was printed in Germany")
+countrys["France"] = ("Western Europe", "France is the birthplace of cinema")
+print(countrys)
 
-var city: [String]
-city = [
-    "Amsterdam",
-    "Copenhagen",
-    "Vilnius",
-]
+// 2
 
-var nameCountru = (["Germany: Western Europe. The surname Hitler is banned in Germany", "Italy: Southern Europe. The ending of all words in Italian only with a vowel", "France: Western Europe. France is the birthplace of the bicycle"])
-let sorting = nameCountru.sorted(by: <)
-print(sorting)
+let filterData = countrys.filter {
+    $0.key.count > 6
+}
 
-var continent: [String: String]
-continent = [
-    "Eurasia": "Austria, Bulgaria, Belgium",
-    "Africa": "Ghana, Ethiopia, Mali",
-    "Australia": "Vanuatu, Nauru, Samoa",
-]
+// 3
 
-let africa = continent["Africa"]
-print(africa)
+let facts = countrys.map {
+"\($0.key): \($0.value.0)"
+}.sorted(by: { $0.count > $1.count })
+print(facts)
+
+//*
+
+let array = [1, 2, 5, 1, 5]
+if array.count == Set(array).count {
+    print("Not identical")
+}
+else {
+    print("Indetical")
+}
